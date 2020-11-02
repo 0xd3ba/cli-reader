83e981d683bb00d50664f9befd5b40f11cafb9a6
Debasish Das <d3ba.1996@gmail.com>
2020-11-02 13:23:21 +0530

    add initial entry and update crawler factory

---
+++ clireader.py
@@ -0,0 +1,11 @@
+# clireader.py -- The entry point of the application
+
+import sys
+
+if __name__ == '__main__':
+
+    # Add new path variables
+    sys.path.append('crawlers/')
+    sys.path.append('shell/')
+
+    # TODO: Start the shell
--- crawlers/cfactory.py
+++ crawlers/cfactory.py
@@ -3,9 +3,23 @@
 # This module contains `CrawlerFactory` which is the factory
 # class for all crawlers that are supported
 
+
+import crawlers.wuxiaworld
+
 class CrawlerFactory:
+
+    # The value of DEFAULT_WEB can only be changed by setweb command
+    DEFAULT_WEB = 'wuxiaworld'
+    SUPPORTED_WEBS = {
+        'wuxiaworld': crawlers.wuxiaworld.WuxiaWorldCrawler
+    }
+
     def __init__(self) -> None:
         raise NotImplementedError
 
-    def get_crawler(self, website):
-        raise NotImplementedError
+    def get_crawler(self, novel):
+        """
+        Return a crawler instance of the default website. We are guaranteed that it's a
+        valid website that is supported.
+        """
+        return self.SUPPORTED_WEBS[self.DEFAULT_WEB](novel)
