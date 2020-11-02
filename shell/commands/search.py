025db634e5a98b20a2c1fb484ea63e4931ea7048
Sagar Tyagi <imsagartyagi@gmail.com>
2020-11-03 02:04:23 +0530

    Update search.py

--- shell/commands/search.py
+++ shell/commands/search.py
@@ -1,6 +1,8 @@
 # search.py -- Searches for a novel in a particular website
 
 import shell.cmdbase
+import crawlers.cfactory as cfactory
+import shlex
 
 class SearchCommand(shell.cmdbase.CommandBase):
     """
@@ -16,13 +18,21 @@ class SearchCommand(shell.cmdbase.CommandBase):
         self.description = self.DESCRIPTION
 
     def help(self):
-        pass
+        return self.__doc__
 
     def execute(self, cmd_args):
-        pass
+        args = self._parse_args(cmd_args)
+        crawler = cfactory.CrawlerFactory()
+        web_crawler = crawler.get_crawler(args.novel)
+        results = web_crawler.search()
+        if results == None:
+            return -1,None
+        return 0,self._parse_result(results)
 
     def _parse_args(self, cmd_args):
-        pass
+        self.arg_parser.add_argument('-n', '--novel', type= str,required=True, help= 'Name of the Novel')
+        args = self.arg_parser.parse_args(shlex.split(cmd_args))
+        return args
 
     def _parse_result(self, result):
         # TODO: Parse the result accordingly
