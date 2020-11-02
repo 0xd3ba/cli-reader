eec5b220c71c57276982f8750ea408b71ab21bb8
Sagar Tyagi <imsagartyagi@gmail.com>
2020-11-03 02:07:51 +0530

    Update listweb.py

--- shell/commands/listwebs.py
+++ shell/commands/listwebs.py
@@ -1,24 +1,29 @@
 # listwebs.py -- Displays all the supported websites
 
 import shell.cmdbase
+import crawlers.cfactory as cfactory
 
 class ListWebsCommand(shell.cmdbase.CommandBase):
     """
     listwebs -- Lists the available websites supported
     """
+    DESCRIPTION = 'List all the websites that are available in the CILREADER'
 
     def __init__(self):
         super().__init__()
+        self.description = self.DESCRIPTION
         # TODO: Add specific instance variables
 
     def help(self):
-        pass
+        return self.__doc__
 
     def execute(self, cmd_args):
-        pass
+        crawler = cfactory.CrawlerFactory()
+        results = crawler.SUPPORTED_WEBS.keys()
+        return self._parse_result(results)
 
     def _parse_args(self, cmd_args):
         pass
 
     def _parse_result(self, result):
-        pass
+        return result
