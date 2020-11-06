37ebb5f25bc7085e617247616219ce960098b197
dreedxerniitb <70070919+dreedxerniitb@users.noreply.github.com>
2020-11-07 00:52:03 +0530

    Update help.py

--- shell/commands/help.py
+++ shell/commands/help.py
@@ -4,6 +4,7 @@ import argparse
 
 import crawlers.cbase
 import shell.cmdbase
+import shell.cmdfactory as cmdfactory
 
 class HelpCommand(shell.cmdbase.CommandBase):
     """
@@ -21,10 +22,23 @@ class HelpCommand(shell.cmdbase.CommandBase):
         return self.__doc__
 
     def execute(self, cmd_args):
-        pass
+        args = self._parse_args(cmd_args)
+        cmdFactoryObj = cmdfactory.CommandFactory()
+        result = {}
+        if len(args) == 0 :
+        	for key in cmdFactoryObj.SUPPORTED_CMDS.keys():
+        		result[key] = cmdFactoryObj.get_command(key).DESCRIPTION
+        	
+        elif len(args) == 1:
+        	result[args[0]] = cmdFactoryObj.get_command(args[0]).help()
+        else:
+        	for cmd in args:
+        		result[cmd] = cmdFactoryObj.get_command(cmd).DESCRIPTION
+        return 1,self_parse_result(result)	#there's no case for status_code 0
 
     def _parse_args(self, cmd_args):
         return cmd_args.split(' ')   # No need of argparse for this command
 
     def _parse_result(self, result):
-        pass
+    	# TODO: Parse the result accordingly
+        return result
