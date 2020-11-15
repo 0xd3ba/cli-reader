9a150584164bfe2db473bda31665bba75cc01a8d
Arnab Das <arnabdas@cse.iitb.ac.in>
2020-11-15 22:11:50 +0530

    Default Command Updated

--- shell/commands/defcmd.py
+++ shell/commands/defcmd.py
@@ -1,12 +1,15 @@
 # defcmd.py -- Default command that is executed when user enters an invalid command
 
 import shell.cmdbase
+from prompt_toolkit import print_formatted_text
 
 
 class DefaultCommand(shell.cmdbase.CommandBase):
     """
-    TODO: Add information about the command
+    DefaultCommand is executed when user inputs some illegal command
     """
+    INVALID_COMMAND_MSG = "Invalid Commands or Arguments. Please enter valid Command. Use Help!"
+    INVALID_COMMAND_STATUS = -1
 
     def __init__(self):
         super().__init__()
@@ -16,10 +19,10 @@ class DefaultCommand(shell.cmdbase.CommandBase):
         pass
 
     def execute(self, cmd_args):
-        pass
+        return self.INVALID_COMMAND_STATUS, self._parse_result(self.INVALID_COMMAND_MSG)
 
     def _parse_args(self, cmd_args):
         pass
 
     def _parse_result(self, result):
-        pass
+        print_formatted_text(result)
